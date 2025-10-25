9bd8b5f954bf97e58973f6fb9d880d841c7a19dc
gabr1eldev <gabriel.gregorio1987@gmail.com>
2025-10-25 18:55:15 -0300

    Creating a CheckingAccountRepository interface

---
+++ src/main/java/br/com/fiap/bank_project/repository/CheckingAccountRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.bank_project.repository;
+
+import br.com.fiap.bank_project.entity.CheckingAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface CheckingAccountRepository extends JpaRepository<CheckingAccount,Long> {
+}
