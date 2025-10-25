3676f5bdfbd6001c44be5bd56fc6fb2358f56a7c
gabr1eldev <gabriel.gregorio1987@gmail.com>
2025-10-25 18:52:52 -0300

    Creating an UserRepository interface

---
+++ src/main/java/br/com/fiap/bank_project/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.bank_project.repository;
+
+import br.com.fiap.bank_project.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface UserRepository extends JpaRepository<User,Long> {
+}
