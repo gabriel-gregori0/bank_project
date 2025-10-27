d481b7ea578640dc7ec4f2bf4afc44f15d12d00c
gabr1eldev <gabriel.gregorio1987@gmail.com>
2025-10-27 12:40:38 -0300

    Creating a new signature in depository and a new findAccountByCpf method

--- src/main/java/br/com/fiap/bank_project/repository/CheckingAccountRepository.java
+++ src/main/java/br/com/fiap/bank_project/repository/CheckingAccountRepository.java
@@ -4,6 +4,9 @@ import br.com.fiap.bank_project.entity.CheckingAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Transactional(readOnly = true)
 public interface CheckingAccountRepository extends JpaRepository<CheckingAccount,Long> {
+    Optional<CheckingAccount> findByCpf(String cpf);
 }
--- src/main/java/br/com/fiap/bank_project/service/serviceImpl/CheckingAccountImpl.java
+++ src/main/java/br/com/fiap/bank_project/service/serviceImpl/CheckingAccountImpl.java
@@ -1,10 +1,15 @@
 package br.com.fiap.bank_project.service.serviceImpl;
 
 import br.com.fiap.bank_project.entity.CheckingAccount;
+import br.com.fiap.bank_project.entity.SavingsAccount;
+import br.com.fiap.bank_project.entity.User;
 import br.com.fiap.bank_project.repository.CheckingAccountRepository;
+import br.com.fiap.bank_project.repository.UserRepository;
 import br.com.fiap.bank_project.service.CheckingAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -13,10 +18,12 @@ public class CheckingAccountImpl implements CheckingAccountService {
 
 
     private final CheckingAccountRepository checkingAccountRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public CheckingAccountImpl(CheckingAccountRepository checkingAccountRepository) {
+    public CheckingAccountImpl(CheckingAccountRepository checkingAccountRepository, UserRepository userRepository) {
         this.checkingAccountRepository = checkingAccountRepository;
+        this.userRepository = userRepository;
     }
 
 
@@ -38,5 +45,23 @@ public class CheckingAccountImpl implements CheckingAccountService {
     @Override
     public List<CheckingAccount> findAll(CheckingAccount account) {
         return List.of();
+    }
+
+    private CheckingAccount findAccountByCpf(String cpf) {
+        User userFound = findByCpf(cpf);
+        CheckingAccount account = new CheckingAccount();
+        account.setUser(userFound);
+
+        return checkingAccountRepository.findByCpf(account.getUser().getCpf())
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                "Usuário não encontrado"));
+    }
+
+    private User findByCpf(String cpf) {
+        return userRepository.findByCpf(cpf)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                "Usuário não encontrado"));
     }
 }
